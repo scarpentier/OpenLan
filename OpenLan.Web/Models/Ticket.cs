2fd4ce3c68a51a15521aefe336bbe907c0b98259
klarcheveque <klarcheveque@gmail.com>
2014-10-25 15:35:25 -0400

    add model

--- OpenLan.Web/Models/Ticket.cs
+++ OpenLan.Web/Models/Ticket.cs
@@ -3,27 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-using Newtonsoft.Json;
-
 namespace OpenLan.Web.Models
 {
-    public class Team
+    public class Ticket
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public ApplicationUser Owner { get; set; }
 
-        public string Url { get; set; }
-
-        public string Token { get; set; }
+        public Transaction transaction { get; set; }
 
         public string Tagline { get; set; }
-
-        public TournamentTeam TournamentTeam { get; set; }
 
         public virtual ICollection<ApplicationUser> Members { get; set; }
 
         public virtual ApplicationUser ContactUser { get; set; }
-
     }
 }
---
+++ OpenLan.Web/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenLan.Web.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public String  Code { get; set; }
+
+        public DateTime TimeStarted { get; set; }
+
+        public DateTime TimeEnded { get; set; }
+
+        public String Status { get; set; }
+
+        public virtual ApplicationUser ContactUser { get; set; }
+    }
+}
