9b3203b6eb6c3d7562f19aa02a16c3c3e7767991
Simon Carpentier <simon.carpentier@spacebar.ca>
2015-03-20 11:41:45 -0400

    Participant Controller & View

---
+++ OpenLan.Web/Controllers/ParticipantController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+using OpenLan.Web.Models;
+using System.Linq;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OpenLan.Web.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly OpenLanContext db;
+
+        public PlayerController(OpenLanContext context)
+        {
+            db = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(db.Users.Include(x => x.Clan));
+        }
+    }
+}
